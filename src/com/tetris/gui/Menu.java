6e7e23a851fc22e18060eae1879a04415bdbf7d0
InaTherese <Ina@trl187068.ka.hist.no>
2011-11-23 21:41:24 +0100

    splash 'n' stuff

--- src/com/tetris/gui/Menu.java
+++ src/com/tetris/gui/Menu.java
@@ -9,17 +9,28 @@ import android.app.Activity;
 import android.content.Intent;
 
 public class Menu extends Activity {
-	 @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.menu);
-	 
-	 Button play = (Button) findViewById(R.id.Button01);
-     play.setOnClickListener(new View.OnClickListener() {
-         public void onClick(View view) {
-             Intent myIntent = new Intent(view.getContext(), TetrisActivity.class);
-             startActivityForResult(myIntent, 0);
-         }
-     });
-}
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu);
+
+		
+		Button play = (Button) findViewById(R.id.Button01);
+		play.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Intent myIntent = new Intent(view.getContext(),
+						TetrisActivity.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
+		
+		Button look = (Button) findViewById(R.id.Button02);
+		look.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				Intent myIntent = new Intent(view.getContext(),
+						ClientActivity.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
+	}
 }
